a483d0d95fa3f7dfbca2ba8ab7604eff80ff50d9
Matthew Miller <milmd90@gmail.com>
2023-06-29 17:34:53 -0700

    working for multiple directories

--- renameTracks/Utility/renameTrack.sh
+++ renameTracks/Utility/renameTrack.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 if [[ "$1" == "" ]]; then
-	echo "missing first argument"
+	echo "Error: Missing first argument"
 	exit
 fi
 if [[ "$2" == "" ]]; then
-	echo "missing second argument"
+	echo "Error: Missing second argument"
 	exit
+fi
+
+if [[ "$3" == "" ]]; then
+	cd ..
+else
+	cd "$3"
+	echo Moved to $PWD
 fi
 
 echo Updating $1 "=>" $2
 
 set -e
-for item1 in ../*; do
+for item1 in *; do
 
 	nextItem=$item1
-	echo 1 nextItem "$nextItem"
 	if [[ "$item1" == "$1"* ]]; then
 		echo HIT1  "$item1"
 		# string replace
@@ -24,12 +30,13 @@ for item1 in ../*; do
 		mv "$item1" "$nextItem"
 	fi
 
+	#echo 1 nextItem "$nextItem"
+
 	if [[ -d "$nextItem" ]] && [[ "$nextItem" != *".logicx/" ]]; then
 		cd "$nextItem"
 		for item2 in *; do
 
 			nextItem=$item2
-			echo 2 nextItem "$nextItem"
 			if [[ "$item2" == $1* ]]; then
 				echo HITT2  "$item2"
 				# string replace
@@ -38,12 +45,13 @@ for item1 in ../*; do
 				mv "$item2" "$nextItem"
 			fi
 
+			#echo 2 nextItem "$nextItem"
+
 			if [[ -d "$nextItem" ]] && [[ "$nextItem" != *".logicx/" ]]; then
 				cd "$nextItem"
 				for item3 in *; do
 
 					nextItem=$item3
-					echo 3 nextItem "$nextItem"
 					if [[ "$item3" == $1* ]]; then
 						echo HITTT3  "$item3"
 						# string replace
@@ -52,12 +60,13 @@ for item1 in ../*; do
 						mv "$item3" "$nextItem"
 					fi
 
+					#echo 3 nextItem "$nextItem"
+
 					if [[ -d "$nextItem" ]] && [[ "$nextItem" != *".logicx/" ]]; then
 						cd "$nextItem"
 						for item4 in *; do
 
 							nextItem=$item4
-							echo 4 nextItem "$nextItem"
 							if [[ "$item4" == $1* ]]; then
 								echo HITTTT4  "$item4"
 								# string replace
@@ -66,12 +75,13 @@ for item1 in ../*; do
 								mv "$item4" "$nextItem"
 							fi
 
+							#echo 4 nextItem "$nextItem"
+
 							if [[ -d "$nextItem" ]] && [[ "$nextItem" != *".logicx/" ]]; then
 								cd "$nextItem"
 								for item5 in *; do
 
 									nextItem=$item5
-									echo 5 nextItem "$nextItem"
 									if [[ "$item5" == $1* ]]; then
 										echo HITTTTT5  "$item5"
 										# string replace
@@ -79,6 +89,8 @@ for item1 in ../*; do
 										echo "5     " $item5 "=>" "$nextItem"
 										mv "$item5" "$nextItem"
 									fi
+
+									#echo 5 nextItem "$nextItem"
 
 								done
 								cd ..
